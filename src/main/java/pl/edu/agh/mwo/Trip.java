795e998d03d16c439d26ebaa474c1ec1373b4203
student9 <student9@pcoip.ki.agh.edu.pl>
2018-03-17 12:12:17 +0100

    Zadanie1 PA2

---
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -0,0 +1,20 @@
+package pl.edu.agh.mwo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Trip {
+	
+	Collection<Photo> photos = new ArrayList <Photo>();
+	
+	public Collection<Photo> getPhotos(){
+		return photos;
+	}
+
+	public Collection<Photo> addPhoto(Photo photo) {
+		
+		return null;
+	}
+	
+
+}
