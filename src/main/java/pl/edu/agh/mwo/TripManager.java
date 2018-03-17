2b5c0e9f4c73f161277d8e4f224ec5321e40871d
student9 <student9@pcoip.ki.agh.edu.pl>
2018-03-17 11:55:49 +0100

    Zadanie1 PA1

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -2,5 +2,10 @@ package pl.edu.agh.mwo;
 
 public class TripManager {
 
+	public Object addTrip(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,5 +1,14 @@
 package pl.edu.agh.mwo;
- 
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class TripManagerTest {
 	
+	@Test
+	public void testAddTrip() {
+		TripManager trip = new TripManager();
+		assertEquals("Trip added: trip1",trip.addTrip("trip1"));
+	}
 }
